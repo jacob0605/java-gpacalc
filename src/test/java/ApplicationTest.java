e9bd53c774a70fb7dab98bc0a72b6c758cdfcfe4
zionhann <hanzion1108@gmail.com>
2024-04-16 14:56:25 +0900

    docs: 요구사항 변경

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,9 +1,9 @@
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-
 import camp.nextstep.edu.missionutils.test.NsTest;
 import gpacalc.Application;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ApplicationTest extends NsTest {
 
@@ -12,14 +12,6 @@ public class ApplicationTest extends NsTest {
         run("데이타구조-3-A0,자바프로그래밍언어-3-B+,컴퓨터구조-3-C0,컴퓨터네트워크-3-D+",
                 "미술의이해-3-P,교양특론3-1-NP,기독교의이해-2-F");
         assertThat(output()).contains(
-                "<과목 목록>",
-                "[전공] 데이타구조,3,A0",
-                "[전공] 자바프로그래밍언어,3,B+",
-                "[전공] 컴퓨터구조,3,C0",
-                "[전공] 컴퓨터네트워크,3,D+",
-                "[교양] 미술의이해,3,P",
-                "[교양] 교양특론3,1,NP",
-                "[교양] 기독교의이해,2,F",
                 "<취득학점>",
                 "15학점",
                 "<평점평균>",
